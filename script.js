65450d7ff49afe2d0085fb0a115520b5d186295d
ManavSandhu09 <manavjitk.sandhu09@gmail.com>
2026-02-01 18:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-let pages = document.querySelectorAll(".page");
-let index = 0;
-
-const music = document.getElementById("bgMusic");
+const pages = document.querySelectorAll(".page");
 const pageSound = document.getElementById("pageSound");
 
-music.volume = 0.2;
-pageSound.volume = 0.15;
+let currentPage = 0;
 
-document.body.addEventListener("click", () => {
-  music.play();
-}, { once: true });
-
-function nextPage() {
-  if (index < pages.length - 1) {
-    // Add exit class to the current page
-    pages[index].classList.add("exit");
-    pages[index].classList.remove("active");
-    
-    index++;
-    
-    // Activate the next page
-    pages[index].classList.add("active");
-    
-    pageSound.currentTime = 0;
+document.addEventListener("click", () => {
+  if (currentPage < pages.length) {
+    pages[currentPage].classList.add("flip");
     pageSound.play();
+    currentPage++;
   }
-}
+});
+
 
 
